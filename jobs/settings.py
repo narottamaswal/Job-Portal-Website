d810b1e3dd76d63739d0c42d65df28fcb2993402
narottamaswal <narottam98678@gmail.com>
2020-08-29 23:23:05 +0530

    2nd

--- jobs/settings.py
+++ jobs/settings.py
@@ -84,23 +84,13 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-  #  'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #   }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '8010project',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   }
 }
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -111,6 +101,16 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.mysql',
+# #         'NAME': '8010project',
+# #         'USER': 'root',
+# #         'PASSWORD': '',
+# #         'HOST': 'localhost',
+# #         'PORT': '3306',
+# #     }
+# # }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
