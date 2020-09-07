fab3e409e481cc2ee25359764887650cd1332e6b
narottamaswal <65083220+narottamaswal@users.noreply.github.com>
2020-09-07 20:08:24 +0530

    Update settings.py

--- jobs/settings.py
+++ jobs/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL= '/media/'
